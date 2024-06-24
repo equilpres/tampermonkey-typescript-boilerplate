de81704051d36b68b427903198d7d353606b8bb1
laytara <laytara@outlook.com>
2024-06-24 14:54:56 +0300

    chore: specify ProcessEnv

--- src/lib/stores/example.ts
+++ src/lib/stores/example.ts
@@ -1,6 +1,6 @@
 import { writable } from 'svelte/store';
 
 export const exampleStore = writable({
-	text: process.env['PUBLIC_EXAMPLE'],
+	text: process.env.PUBLIC_EXAMPLE,
 	createdAt: new Date().toISOString(),
 });
--- typings.d.ts
+++ typings.d.ts
@@ -8,4 +8,10 @@ declare namespace svelteHTML {
 	}
 }
 
+declare namespace NodeJS {
+	interface ProcessEnv {
+		EXAMPLE: string;
+	}
+}
+
 declare const Meta: Exclude<Readonly<import('./src/meta.d.ts').Meta>, 'match' | 'grant' | 'noframes' | 'webRequest'>;
